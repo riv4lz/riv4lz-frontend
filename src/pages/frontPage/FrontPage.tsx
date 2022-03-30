b6ffd237a0e1464b0da9a7c2b05f63a91abbca9a
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-03-30 04:07:32 +0200

    News component
    Mangler "NEWS" i siden, men er ikke sikker på hvordan det skal laves.

--- src/pages/frontPage/FrontPage.tsx
+++ src/pages/frontPage/FrontPage.tsx
@@ -3,6 +3,7 @@ import CallToAction from '../../components/frontpage/CallToAction/CallToAction'
 import Games from '../../components/frontpage/Games/Games'
 import Guide from '../../components/frontpage/Guide/Guide'
 import Mission from '../../components/frontpage/Mission/Mission'
+import News from '../../components/frontpage/News/News'
 
 const FrontPage = () => {
     return (
@@ -11,6 +12,7 @@ const FrontPage = () => {
             <Guide />
             <Mission />
             <Games />
+            <News />
         </>
     )
 }
