7f29e260926bba027be6055e2eab98adb794c3cb
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-04-20 08:16:03 +0200

    added smooth scroll on feature

--- src/components/frontpage/CallToAction/CallToAction.tsx
+++ src/components/frontpage/CallToAction/CallToAction.tsx
@@ -1,39 +1,46 @@
 import React from 'react'
+import { Link, Route, useNavigate } from 'react-router-dom'
 import Btn from '../../button/Btn'
 import './CallToAction.scss'
 
 const CallToAction = () => {
-    const onBecomMember = () =>{
-        console.log("test");
-    }  
+    const navigate = useNavigate();
 
-  return (
-      <div className='callToAction_Container'>
-          <div className='callToAction_Wrapper'>
-            <div className='callToAction_MainSection'>
-                <div className='callToAction_Title H1 Text_Secondary'>
-                    CASTING MADE    
+    const onBecomMember = () => {
+        console.log("test");
+    }
+
+    const onFeatures = () => {
+        navigate('#Features');
+    }
+
+    return (
+        <div className='callToAction_Container'>
+            <div className='callToAction_Wrapper'>
+                <div className='callToAction_MainSection'>
+                    <div className='callToAction_Title H1 Text_Secondary'>
+                        CASTING MADE
+                    </div>
+
+                    <div className='callToAction_TitleModern A3 Text_Primary' style={{ fontSize: '105px' }}>
+                        MODERN
+                    </div>
+                    <div className='callToAction_Paragraph P3_Statewide_Bold'>
+                        <span className='Text_Secondary'>Discover our innovative platform for casters and organisations alike - </span>
+                        <span className='Text_Primary'>“imagine if booking a caster was as easy as ordering a pizza”</span>
+
+                    </div>
+                    <div className='callToAction_Buttons'>
+                        <Btn onClick={onBecomMember} children='BECOME A MEMBER' classes='btn_CallToAction_Solid P4_Statewide_Bold Text_Secondary'></Btn>
+                        <Btn onClick={onFeatures} children='EXPLORE FEATURES' classes='btn_CallToAction_Outline P4_Statewide_Bold Text_Secondary'></Btn>
+                    </div>
                 </div>
-                
-                <div className='callToAction_TitleModern A3 Text_Primary' style={{fontSize: '105px' }}>
-                    MODERN
-                </div>
-                <div className='callToAction_Paragraph P3_Statewide_Bold'>
-                    <span className='Text_Secondary'>Discover our innovative platform for casters and organisations alike - </span>
-                    <span className='Text_Primary'>“imagine if booking a caster was as easy as ordering a pizza”</span>  
-                     
-                </div>
-                <div className='callToAction_Buttons'>
-                    <Btn onClick={onBecomMember} children='BECOME A MEMBER' classes='btn_CallToAction_Solid P4_Statewide_Bold Text_Secondary'></Btn>
-                    <Btn onClick={onBecomMember} children='EXPLORE FEATURES' classes='btn_CallToAction_Outline P4_Statewide_Bold Text_Secondary'></Btn>
+                <div className='callToAction_emptyDiv'>
+                    &nbsp;
                 </div>
             </div>
-            <div className='callToAction_emptyDiv'>
-                &nbsp;
-            </div>
-          </div>
-      </div>
-  )
+        </div >
+    )
 }
 
 export default CallToAction
