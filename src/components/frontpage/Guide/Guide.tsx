a2f414079d71e7070760853a5953b743ba0ed168
Frann0 <deengo113@gmail.com>
2022-04-22 08:22:28 +0200

    Guide progress

--- src/components/frontpage/Guide/Guide.tsx
+++ src/components/frontpage/Guide/Guide.tsx
@@ -1,80 +1,84 @@
 import React from 'react'
 import Btn from '../../button/Btn'
 import './Guide.scss'
+import "../../../index.scss";
 
 const Guide = () => {
+    var caster: boolean = true;
+    var org: boolean = false;
+
     const test = () => {
         console.log("TEST 1");
 
     }
 
+    const onCaster = () => {
+        caster = !caster;
+
+        console.log(caster);
+        console.log(org);
+
+
+    }
+
+    const onOrg = () => {
+        caster = false;
+        org = true;
+    }
+
+
     return (
         <div id="Features" className='guide_Container'>
             <div className='guide_Wrapper'>
-                <div className='guide_Title H2 Text_Secondary'>
-                    HOW DOES IT WORK?
-                </div>
-                <div className='guide_StepCards'>
-                    <div className='guide_StepContainer'>
-                        <div className='guide_StepWrapper'>
-                            <div className='guide_stepCounter A2 Text_Secondary'>
-                                1
+                <div>
+                    <div className='guide_Title H2 Text_Secondary'> how does it work?</div>
+                    <div className='guide_Buttons'>
+                        <div>Caster</div>
+                        <div>Organisation</div>
+                    </div>
+                    <div className='guide_ComponentContainer Flex Justify_Center Align_Center'>
+                        <div className='number A4 Text_Secondary Flex Justify_Center Align_Center'>
+                            1
+                        </div>
+                        <div className='text_Wrapper Flex'>
+                            <div className='title P1_Poppins Text_Secondary'>
+                                Register
                             </div>
-                            <div className='guide_stepCounter_Text'>
-                                <div className='title P1_Statewide_Bold Text_Secondary'>
-                                    STEP 1
-                                </div>
-                                <div className='paragraph P4_Poppins Text_Secondary'>
-                                    The platform is designed to work as a media for orginasations to easily find_in_page
-                                    casters but also for casters to find jobs and essentially to suport the esport industry
-                                    in general
-                                </div>
+                            <div className='paragraph P3_Poppins Text_Secondary'>
+                                For business which a product online
+                                product listings in the
                             </div>
                         </div>
                     </div>
-                    <div className='guide_StepContainer Float_Right'>
-                        <div className='guide_StepWrapper '>
-                            <div className='guide_stepCounter A2 Text_Secondary'>
-                                2
+                    <div className='guide_ComponentContainer Flex Justify_Center Align_Center'>
+                        <div className='number A4 Text_Secondary Flex Justify_Center Align_Center'>
+                            2
+                        </div>
+                        <div className='text_Wrapper Flex'>
+                            <div className='title P1_Poppins Text_Secondary'>
+                                Find Match
                             </div>
-                            <div className='guide_stepCounter_Text'>
-                                <div className='title P1_Statewide_Bold Text_Secondary'>
-                                    STEP 2
-                                </div>
-                                <div className='paragraph P4_Poppins Text_Secondary'>
-                                    The platform is designed to work as a media for orginasations to easily find_in_page
-                                    casters but also for casters to find jobs and essentially to suport the esport industry
-                                    in general
-                                </div>
+                            <div className='paragraph P3_Poppins Text_Secondary'>
+                                For business which a product online
+                                product listings in the
                             </div>
                         </div>
                     </div>
-                    <div className='guide_StepContainer'>
-                        <div className='guide_StepWrapper'>
-                            <div className='guide_stepCounter A2 Text_Secondary'>
-                                3
+                    <div className='guide_ComponentContainer Flex Justify_Center Align_Center'>
+                        <div className='number A4 Text_Secondary Flex Justify_Center Align_Center'>
+                            3
+                        </div>
+                        <div className='text_Wrapper Flex'>
+                            <div className='title P1_Poppins Text_Secondary'>
+                                Make Offer
                             </div>
-                            <div className='guide_stepCounter_Text'>
-                                <div className='title P1_Statewide_Bold Text_Secondary'>
-                                    STEP 3
-                                </div>
-                                <div className='paragraph P4_Poppins Text_Secondary'>
-                                    The platform is designed to work as a media for orginasations to easily find_in_page
-                                    casters but also for casters to find jobs and essentially to suport the esport industry
-                                    in general
-                                </div>
+                            <div className='paragraph P3_Poppins Text_Secondary'>
+                                For business which a product online
+                                product listings in the
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className='guide_VideoContainer'>
-                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" ></iframe>
-                </div>
-                <div className='guide_ButtonsContainer'>
-                    <div className='guide_ButtonsWrapper'>
-                        <Btn onClick={test} children='SEE OUR FULL GUIDE' classes='btn_CallToAction_Solid P4_Statewide_Bold Text_Secondary'></Btn>
-                        <Btn onClick={test} children='READ MORE ABOUT US' classes='btn_CallToAction_Outline P4_Statewide_Bold Text_Secondary'></Btn>
-                    </div>
+
                 </div>
             </div>
         </div>
