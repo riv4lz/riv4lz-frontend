4dbbdbfad39914b4f23324897c4d555d813c4a32
Frann0 <deengo113@gmail.com>
2022-05-01 20:01:48 +0200

    Blog added

---
+++ src/components/frontpage/Blog/Blog.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <div className='Blog_Container'>
+            <div className='Blog_Wrapper'>
+                <div className='Blog_Title'> Our Latest Updates</div>
+                <div className='Blog_Paragraph'>Conveniently customize proactive services leveraged without
+                    continualliery customize continually.
+                </div>
+                <div className='Component_Container'>
+                    <div className='Component_Wrapper'>
+                        <div className='Image'>
+                            Image
+                        </div>
+                        <div className='Title'>Title</div>
+                        <div className='Paragraph'>Paragraph</div>
+                        <div className='Read_More'>Read More</div>
+                    </div>
+                    <div className='Component_Wrapper'>
+                        <div className='Image'>
+                            Image
+                        </div>
+                        <div className='Title'>Title</div>
+                        <div className='Paragraph'>Paragraph</div>
+                        <div className='Read_More'>Read More</div>
+                    </div>
+                    <div className='Component_Wrapper'>
+                        <div className='Image'>
+                            Image
+                        </div>
+                        <div className='Title'>Title</div>
+                        <div className='Paragraph'>Paragraph</div>
+                        <div className='Read_More'>Read More</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Blog
