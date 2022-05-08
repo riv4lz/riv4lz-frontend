e83c8fc7c896a6d53859c87c477ed585b1f2e725
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-05-08 22:00:27 +0200

    Updated for org
    Skal hookes up, men ellers good to go tænker jeg

--- src/components/shared/Event/EventDetails/EventDetails.tsx
+++ src/components/shared/Event/EventDetails/EventDetails.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import './EventDetails.scss'
 import Tricked from '../../../../assets/images/Esports-orgs/Tricked.svg'
 import Btn from '../../../button/Btn'
 
 const EventDetails = ({ Event, handleClose, show }: any) => {
+    const [casterState, setCasterState] = useState(false);
+    const [organisationState, setOrganisationState] = useState(true);
     return (
         <div className='Event_Container' style={{ display: show ? 'block' : 'none' }}>
             <div className='Event_Wrapper Flex Text_Secondary'>
@@ -66,6 +68,8 @@ const EventDetails = ({ Event, handleClose, show }: any) => {
                         </div>
                     </div>
                 </div>
+                {casterState ? <Caster /> : null}
+                {organisationState ? <Organisation /> : null}
                 <div className='Event_button Flex Justify_Center Align_Center'>
                     <Public Close={handleClose} />
                 </div>
@@ -76,6 +80,92 @@ const EventDetails = ({ Event, handleClose, show }: any) => {
 
 
 const Public = ({ Close }: any) => {
+    return (
+        <Btn onClick={Close} children='Close' classes='btn_CallToAction_Blue P3_Oxanium Bold Text_Dark_Blue' />
+    )
+}
+
+const Caster = () => {
+    return (
+        <div className='Caster_Container Flex Justify_Center Align_Center'> </div>
+    )
+}
+
+const CasterButtons = ({ Close }: any) => {
+    return (
+        <Btn onClick={Close} children='Close' classes='btn_CallToAction_Blue P3_Oxanium Bold Text_Dark_Blue' />
+    )
+}
+const Organisation = () => {
+    const offers: any[] = [{
+        caster: {
+            ProfileImage: 'https://i.imgur.com/lpWSQZX_d.webp?maxwidth=760&fidelity=grand',
+            firstname: 'John',
+            lastname: 'Doe',
+            gamerTag: 'JohnDoe',
+            description: 'Lorem ipsum dolor sit amet',
+        },
+        casterOffer: '10$'
+    }, {
+        caster: {
+            ProfileImage: 'https://i.imgur.com/lpWSQZX_d.webp?maxwidth=760&fidelity=grand',
+            firstname: 'John',
+            lastname: 'Doe',
+            gamerTag: 'JohnDoe',
+            description: 'Lorem ipsum dolor sit amet',
+        },
+        casterOffer: '10$'
+    }, {
+        caster: {
+            ProfileImage: 'https://i.imgur.com/lpWSQZX_d.webp?maxwidth=760&fidelity=grand',
+            firstname: 'John',
+            lastname: 'Doe',
+            gamerTag: 'JohnDoe',
+            description: 'Lorem ipsum dolor sit amet',
+        },
+        casterOffer: '10$'
+    }, {
+        caster: {
+            ProfileImage: 'https://i.imgur.com/lpWSQZX_d.webp?maxwidth=760&fidelity=grand',
+            firstname: 'John',
+            lastname: 'Doe',
+            gamerTag: 'JohnDoe',
+            description: 'Lorem ipsum dolor sit amet',
+        },
+        casterOffer: '10$'
+    }];
+
+    return (
+        <div className='Offer_Container Flex Justify_Center Align_Center'>
+            <div className='Offer_Title Flex Justify_Start Align_Center P1_Statewide_Bold Text_Secondary'>Caster Applications</div>
+            <div className='Component_Container Flex Align_Start'>
+                {offers.map((offer: any) => {
+                    return (
+                        <div className='Component_Wrapper Flex Justify_Start Align_Center'>
+                            <div className='image Flex Justify_Center Align_Center'> <img src={offer.caster.ProfileImage} alt="" /></div>
+                            <div className='name_container Flex Justify_Around Align_Center'>
+                                <div className='name P1_Statewide_Bold Text_Secondary'>
+                                    {offer.caster.firstname} {offer.caster.lastname} - {offer.caster.gamerTag}
+                                </div>
+                                <div className='description P4_Statewide_Bold Text_Secondary'>
+                                    {offer.caster.description}
+                                </div>
+                            </div>
+                            <div className='offer_container Flex Justify_End Align_Center'>
+                                <div className='offer P1_Statewide_Bold Text_Secondary'>{offer.casterOffer}</div>
+                                <div className='accept btn_Offer_Solid'>Accept</div>
+                                <div className='decline btn_Offer_Outline'>Decline</div>
+                            </div>
+                        </div>
+                    )
+                })}
+
+            </div>
+        </div>
+    )
+}
+
+const OrganisationButtons = ({ Close }: any) => {
     return (
         <Btn onClick={Close} children='Close' classes='btn_CallToAction_Blue P3_Oxanium Bold Text_Dark_Blue' />
     )
