aedfa273218d0bfc2e33ef7466830f268e348bd0
FrederikOtto <frederik.skarbyottosen@gmail.com>
2022-05-24 15:16:59 +0200

    Refac Footer

---
+++ src/components/shared/Footer/CopyrightContainer/CopyrightContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CopyrightContainer.scss'
+
+
+const CopyrightContainer = () => {
+    return (
+            <div className='[ CopyrightContainer ]     { Flex Justify_Center Align_Center P3_Oxanium Semi_Bold Text_Secondary'>
+                © Copyright 2022. All Rights Reserved by Riv4lz ApS
+            </div>
+    )
+}
+
+export default CopyrightContainer
