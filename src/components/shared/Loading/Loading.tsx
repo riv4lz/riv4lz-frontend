25a328ae0a889ae595e7cd44b3fc5119879f401a
Frann0 <deengo113@gmail.com>
2022-06-15 18:03:13 +0200

    Stroke width error

--- src/components/shared/Loading/Loading.tsx
+++ src/components/shared/Loading/Loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
             <div className='Loading__Wrapper    { flex-flow }'>
                 <div className='Loading__Spinner    { height-full width-full }'>
                     <svg id="loading-spinner" width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle id="loading-circle-large" cx="40" cy="40" r="36" stroke-width="8" />
+                        <circle id="loading-circle-large" cx="40" cy="40" r="36" strokeWidth="8" />
                     </svg>
                 </div>
             </div>
